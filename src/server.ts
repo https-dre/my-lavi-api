5e2db8f7ac876973bae562fe70c9a664c86718ca
André Dias <diaso.andre@outlook.com>
2025-08-06 11:04:16 +0000

    refactor: add scalar api reference and environment checking

--- src/server.ts
+++ src/server.ts
@@ -6,45 +6,49 @@ import {
 } from "fastify-type-provider-zod";
 import fastifyMultipart from "@fastify/multipart";
 import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import config from "./env-config";
+import { verify_env } from "./env-config";
 import { pingDatabase } from "./db/conn";
-import EnvConfig from './env-config';
+import { logger } from "./logger";
 
 const app = fastify({
 	ignoreTrailingSlash: true,
 });
 
-const run = async () => {
-	await pingDatabase();
-	await app.register(fastifyMultipart);
-
-	await app.register(fastifySwagger, {
-		swagger: {
-			consumes: ["application/json", "multipart/form-data"],
-			produces: ["application/json"],
-			info: {
-				title: "Laundry API Docs",
-				description: "...",
-				version: "1.0.0",
-			},
+app.register(fastifyMultipart);
+app.register(fastifySwagger, {
+	swagger: {
+		consumes: ["application/json", "multipart/form-data"],
+		produces: ["application/json"],
+		info: {
+			title: "Laví API Docs",
+			description: "...",
+			version: "1.0.0",
 		},
-		transform: jsonSchemaTransform,
-	});
+	},
+	transform: jsonSchemaTransform,
+})
 
-	await app.register(fastifySwaggerUi, {
-		routePrefix: "/docs",
+const run = async () => {
+	verify_env();
+	await pingDatabase();
+
+	await app.register(import("@scalar/fastify-api-reference"), {
+		routePrefix: "/docs"
 	});
 
 	app.setValidatorCompiler(validatorCompiler);
 	app.setSerializerCompiler(serializerCompiler);
 
-	const port_from_env = EnvConfig.get('PORT')
+	const port_from_env = process.env.PORT!
 	const current_port = port_from_env ? port_from_env : "5555"
 
-	app.listen({ port: current_port }).then(() => {
-		console.log(`Server running => PORT: ${current_port}`);
-	});
+	app.listen({ port: Number(current_port) }, (err, address) => {
+		if (err) {
+			throw err;
+		}
+
+		logger.info("Server running at: " + address)
+	})
 };
 
 run();
