b6d63efceb989ff1365f8a527b624b5786c14afd
André Dias <diaso.andre@outlook.com>
2025-08-06 11:12:09 +0000

    feat: add a function to check database connection

--- src/drizzle/conn.ts
+++ src/drizzle/conn.ts
@@ -1,3 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { logger } from "../logger";
 
 export const db = drizzle(process.env.DATABASE_URL!);
+
+export const check_db = async () => {
+  logger.info("Checking database connection...");
+  await db.execute("SELECT 1");
+  logger.info("Database connection ok!");
+
+}
