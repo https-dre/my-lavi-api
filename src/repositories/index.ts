cf04651d0906c8fedbdc1b1f4539dce77274acba
https-dre <diaso.andre@outlook.com>
2025-08-06 19:16:03 -0300

    feat(owner): add owner repository interface

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,9 +1,22 @@
-import { CustomerModel } from "../models";
+import { CustomerModel, OwnerModel } from "../models";
 
 export interface ICustomerRepository {
   save(data: Omit<CustomerModel, "id">): Promise<void>;
   delete(id: string): Promise<void>;
   findByEmail(email: string): Promise<CustomerModel>;
   findById(id: string): Promise<CustomerModel>;
-  update(updates: Partial<Omit<CustomerModel, "id">>, id: string): Promise<void>;
+  update(
+    updates: Partial<Omit<CustomerModel, "id">>,
+    id: string
+  ): Promise<void>;
 }
+
+export interface IOwnerRepository {
+  save(data: Omit<OwnerModel, "id">): Promise<OwnerModel>;
+  delete(id: string): Promise<void>;
+  findByEmail(email: string): Promise<OwnerModel>;
+  findById(id: string): Promise<OwnerModel>;
+  findByCpf(cpf: string): Promise<OwnerModel>;
+  update(updates: Partial<Omit<OwnerModel, "id">>, id: string): Promise<void>;
+}
+
