8e83c967d96b56e94e6ff272a39ed2ac26356907
https-dre <diaso.andre@outlook.com>
2025-08-06 19:14:30 -0300

    feat: add drizzle migrations

---
+++ drizzle/0001_orange_vivisector.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "owner" ADD COLUMN "password" text;
---
+++ drizzle/0002_luxuriant_sentinel.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "owner" ALTER COLUMN "name" SET DATA TYPE varchar;--> statement-breakpoint
+ALTER TABLE "owner" ALTER COLUMN "cpf" SET DATA TYPE char;--> statement-breakpoint
+ALTER TABLE "owner" ALTER COLUMN "cep" SET DATA TYPE char;
