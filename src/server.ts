f5ca20f5d0c089a2b78c1ff7022cf22a919930c8
https-dre <diaso.andre@outlook.com>
2025-08-06 22:15:53 -0300

    feat(api): add error handler
    - the prettier styles the file automatically

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import fastify from "fastify";
 import {
-	validatorCompiler,
-	serializerCompiler,
-	jsonSchemaTransform,
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
 import fastifyMultipart from "@fastify/multipart";
 import fastifySwagger from "@fastify/swagger";
@@ -10,51 +10,54 @@ import { verify_env } from "./env-config";
 import { logger } from "./logger";
 import { check_db } from "./drizzle/conn";
 import { router } from "./routes";
+import { ServerErrorHandler } from "./error-handler";
 
 const app = fastify({
-	ignoreTrailingSlash: true,
+  ignoreTrailingSlash: true,
 });
 
 app.register(fastifyMultipart);
 app.register(fastifySwagger, {
-	swagger: {
-		consumes: ["application/json", "multipart/form-data"],
-		produces: ["application/json"],
-		info: {
-			title: "Laví API Docs",
-			description: "...",
-			version: "1.0.0",
-		},
-	},
-	transform: jsonSchemaTransform,
-})
+  swagger: {
+    consumes: ["application/json", "multipart/form-data"],
+    produces: ["application/json"],
+    info: {
+      title: "Laví API Docs",
+      description: "...",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.setErrorHandler(ServerErrorHandler);
 
 const run = async () => {
-	verify_env();
-	await check_db();
+  verify_env();
+  await check_db();
 
-	app.register(router);
+  app.register(router);
 
-	await app.register(import("@scalar/fastify-api-reference"), {
-		routePrefix: "/docs",
-		configuration: {
-			theme: 'kepler'
-		}
-	});
+  await app.register(import("@scalar/fastify-api-reference"), {
+    routePrefix: "/docs",
+    configuration: {
+      theme: "kepler",
+    },
+  });
 
-	app.setValidatorCompiler(validatorCompiler);
-	app.setSerializerCompiler(serializerCompiler);
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
 
-	const port_from_env = process.env.PORT!
-	const current_port = port_from_env ? port_from_env : "5555"
+  const port_from_env = process.env.PORT!;
+  const current_port = port_from_env ? port_from_env : "5555";
 
-	app.listen({ port: Number(current_port) }, (err, address) => {
-		if (err) {
-			throw err;
-		}
+  app.listen({ port: Number(current_port) }, (err, address) => {
+    if (err) {
+      throw err;
+    }
 
-		logger.info("Server running at: " + address)
-	})
+    logger.info("Server running at: " + address);
+  });
 };
 
 run();
