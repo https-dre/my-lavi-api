0c07ab1dba14d227dbed1a2843a701f1e222a421
André Dias <diaso.andre@outlook.com>
2025-08-08 17:40:56 +0000

    feat(docs,reference): add api reference based on zod type provider

--- src/dto/zod-schemas.ts
+++ src/dto/zod-schemas.ts
@@ -1,12 +1,12 @@
 import z from "zod";
 
 export const ZodOwner = z.object({
-  id: z.string(),
+  id: z.string().uuid(),
   name: z.string(),
-  cpf: z.string(),
+  cpf: z.string().length(11),
   verified: z.boolean(),
-  email: z.string(),
+  email: z.string().email(),
   password: z.string(),
   birth_date: z.string(),
-  cep: z.string()
+  cep: z.string().length(8)
 });
---
+++ src/routes/schemas/owner-api.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+import { ZodOwner } from "../../dto/zod-schemas";
+
+export const create_owner = {
+  summary: "Create a owner",
+  body: z.object({
+    owner: ZodOwner.omit({ id: true})
+  }),
+  response: {
+    201: z.object({
+      details: z.string(),
+      owner_id: z.string().uuid()
+    })
+  }
+}
