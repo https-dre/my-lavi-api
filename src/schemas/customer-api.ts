896eb0e2dfecd1bc9f36604a8d29262d1d4b5f6e
https-dre <diaso.andre@outlook.com>
2025-08-08 18:06:48 -0300

    feat(customer): add route schema for authentication

--- src/schemas/customer-api.ts
+++ src/schemas/customer-api.ts
@@ -3,12 +3,28 @@ import { ZodCustomer } from "../dto/zod-schemas";
 
 export const create_customer = {
   summary: "Create a customer",
+  tags: ["customer"],
   body: z.object({
     customer: ZodCustomer.omit({ id: true, created_at: true })
   }),
   response: {
     201: z.object({
       customer_id: z.string().uuid()     
+    })
+  }
+}
+
+export const auth_customer = {
+  summary: "Authenticate a customer by password",
+  tags: ["customer"],
+  body: z.object({
+    email: z.string().email(),
+    password: z.string()
+  }),
+  response: {
+    200: z.object({
+      details: z.string(),
+      token: z.string()
     })
   }
 }
