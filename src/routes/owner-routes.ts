f6105e658b1c1f70fa7a69c5f71a414c8223d7c9
André Dias <diaso.andre@outlook.com>
2025-08-08 17:42:24 +0000

    feat(docs,reference): implement schema for create owner route

--- src/routes/owner-routes.ts
+++ src/routes/owner-routes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { OwnerRepository } from "../repositories/owner-repository";
 import { OwnerController } from "../controller/owner-controller";
+import { create_owner } from "./schemas/owner-api";
 
 export const owner_routes = (app: FastifyInstance) => {
   const ownerRepository = new OwnerRepository();
   const ownerController = new OwnerController(ownerRepository);
 
-  app.post("/owners", {}, ownerController.save.bind(ownerController));
+  app.post("/owners", { schema: create_owner}, ownerController.save.bind(ownerController));
 };
