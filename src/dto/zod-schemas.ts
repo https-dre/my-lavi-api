fa610dadec28b9388e423e3bfbcb5d5d1331a614
https-dre <diaso.andre@outlook.com>
2025-08-08 17:36:26 -0300

    feat(repository): add email to zod customer schema and fix schemas

--- src/dto/zod-schemas.ts
+++ src/dto/zod-schemas.ts
@@ -8,17 +8,18 @@ export const ZodOwner = z.object({
   email: z.string().email(),
   password: z.string(),
   birth_date: z.string(),
-  cep: z.string().length(8)
+  cep: z.string().length(8),
 });
 
 export const ZodCustomer = z.object({
   id: z.string().uuid(),
   profile_url: z.string().url().optional(),
   name: z.string(),
-  is_pg: z.boolean(),
+  email: z.string().email(),
+  is_pj: z.boolean(),
   doc: z.string().max(14).min(11),
   birth_date: z.string(),
   gender: z.string(),
   password: z.string(),
-  created_at: z.string()
-})
+  created_at: z.string(),
+});
