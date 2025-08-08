2b49c1a559251e80ef8507880b93bf2b4e07a731
https-dre <diaso.andre@outlook.com>
2025-08-08 18:09:23 -0300

    feat(customer): add route for authentication on router

--- src/routes/customer-routes.ts
+++ src/routes/customer-routes.ts
@@ -1,11 +1,21 @@
 import { FastifyInstance } from "fastify";
 import { CustomerRepository } from "../repositories/customer-repository";
 import { CustomerController } from "../controller/customer-controller";
-import { create_customer } from "../schemas/customer-api";
+import { auth_customer, create_customer } from "../schemas/customer-api";
 
 export const customer_routes = (app: FastifyInstance) => {
   const customerRepository = new CustomerRepository();
   const customerController = new CustomerController(customerRepository);
 
-  app.post("/customers", { schema: create_customer}, customerController.save.bind(customerController));
-}
+  app.post(
+    "/customers",
+    { schema: create_customer },
+    customerController.save.bind(customerController)
+  );
+
+  app.put(
+    "/customers/sign",
+    { schema: auth_customer },
+    customerController.authByPassword.bind(customerController)
+  );
+};
