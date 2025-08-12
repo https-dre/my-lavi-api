76ffaeb4002d04dd4c7b3e82a14fb4187124df63
André Dias <diaso.andre@outlook.com>
2025-08-12 20:12:10 +0000

    feat: add customer dto

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,5 +1,7 @@
 // Data Transfer Object
 import { RemoveSha256Fields } from '../functions/remove-sensitive-fields';
-import { OwnerModel } from '../models';
+import { CustomerModel, OwnerModel } from '../models';
 
 export type OwnerDTO = RemoveSha256Fields<OwnerModel>;
+
+export type CustomerDTO = RemoveSha256Fields<CustomerModel>;
