a82926b7c12881455cefb1482833fbc0d625fd5f
André Dias <diaso.andre@outlook.com>
2025-08-12 11:40:53 +0000

    tests: add basic e2e

---
+++ tests/e2e/ping.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, beforeAll, afterAll, expect } from "vitest";
+import request from "supertest";
+import { buildApp } from "../../src/buildApp";
+
+let app: ReturnType<typeof buildApp>;
+
+beforeAll(async () => {
+  app = buildApp();
+  await app.ready();
+})
+
+afterAll(async () => {
+  await app.close();
+})
+
+describe("GET /ping", () => {
+  it('should return { pong: true }', async () => {
+    const response = await request(app.server).get('/ping');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ pong: true })
+  })
+})
