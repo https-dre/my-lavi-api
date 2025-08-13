685aa282e9975b94e09a9b235dcb29e637a9854d
https-dre <diaso.andre@outlook.com>
2025-08-12 22:41:32 -0300

    refactor(customer): implement service and provider for customers

--- src/routes/customer-routes.ts
+++ src/routes/customer-routes.ts
@@ -1,11 +1,21 @@
 import { FastifyInstance } from "fastify";
 import { CustomerRepository } from "../repositories/customer-repository";
 import { CustomerController } from "../controller/customer-controller";
-import { auth_customer, create_customer, update_customer } from "../schemas/customer-api";
+import {
+  auth_customer,
+  create_customer,
+  update_customer,
+} from "../schemas/customer-api";
+import { CustomerService } from "../services/customer-service";
+import { CryptoProvider, JwtProvider } from "../providers/crypto-provider";
 
 export const customer_routes = (app: FastifyInstance) => {
-  const customerRepository = new CustomerRepository();
-  const customerController = new CustomerController(customerRepository);
+  const customerService = new CustomerService(
+    new CustomerRepository(),
+    new CryptoProvider(),
+    new JwtProvider()
+  );
+  const customerController = new CustomerController(customerService);
 
   app.post(
     "/customer",
@@ -16,12 +26,12 @@ export const customer_routes = (app: FastifyInstance) => {
   app.put(
     "/customer/sign",
     { schema: auth_customer },
-    customerController.authByPassword.bind(customerController)
+    customerController.auth.bind(customerController)
   );
 
   app.put(
     "/customer/update",
     { schema: update_customer },
     customerController.update.bind(customerController)
-  )
+  );
 };
