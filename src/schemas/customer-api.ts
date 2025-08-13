9e3e5e44abf3ceace7d807f00b2dcf553c95bc34
https-dre <diaso.andre@outlook.com>
2025-08-12 21:13:08 -0300

    styling

--- src/schemas/customer-api.ts
+++ src/schemas/customer-api.ts
@@ -5,37 +5,41 @@ export const create_customer = {
   summary: "Create a customer",
   tags: ["customer"],
   body: z.object({
-    customer: ZodCustomer.omit({ id: true, created_at: true })
+    customer: ZodCustomer.omit({ id: true, created_at: true }),
   }),
   response: {
     201: z.object({
-      customer_id: z.string().uuid()     
-    })
-  }
-}
+      customer_id: z.string().uuid(),
+    }),
+  },
+};
 
 export const auth_customer = {
   summary: "Authenticate a customer by password",
   tags: ["customer"],
   body: z.object({
     email: z.string().email(),
-    password: z.string()
+    password: z.string(),
   }),
   response: {
     200: z.object({
       details: z.string(),
-      token: z.string()
-    })
-  }
-}
+      token: z.string(),
+    }),
+  },
+};
 
 export const update_customer = {
   summary: "Update fields of customer",
   tags: ["customer"],
   body: z.object({
-    fields: ZodCustomer.omit({ id: true, created_at: true, password: true }).partial()
+    fields: ZodCustomer.omit({
+      id: true,
+      created_at: true,
+      password: true,
+    }).partial(),
   }),
   params: z.object({
-    id: z.string().uuid()
-  })
-}
+    id: z.string().uuid(),
+  }),
+};
