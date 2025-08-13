0daddb9d58ea8cea491d385e7d21338338962103
https-dre <diaso.andre@outlook.com>
2025-08-13 18:11:16 -0300

    styling: prettier

--- src/controller/owner-controller.ts
+++ src/controller/owner-controller.ts
@@ -4,23 +4,23 @@ import z from "zod";
 import { auth_owner, create_owner } from "../schemas/owner-api";
 
 export class OwnerController {
-  constructor(readonly service: OwnerService) {};
+  constructor(readonly service: OwnerService) {}
 
   async save(req: FastifyRequest, reply: FastifyReply) {
     const { owner } = req.body as z.infer<typeof create_owner.body>;
     const saved_owner = await this.service.saveOwner(owner);
-    return reply.code(201).send({ 
+    return reply.code(201).send({
       details: "Cadastro realizado com sucesso!",
-      owner_id: saved_owner.id
+      owner_id: saved_owner.id,
     });
   }
 
   async auth(req: FastifyRequest, reply: FastifyReply) {
     const { email, password } = req.body as z.infer<typeof auth_owner.body>;
     const token = await this.service.authenticateOwner(email, password);
-    return reply.code(200).send({ 
+    return reply.code(200).send({
       details: "Autenticado com sucesso!",
-      token
-    }) 
+      token,
+    });
   }
 }
