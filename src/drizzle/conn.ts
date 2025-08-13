95cad07159123c3967c4217d179e18b190bb6610
https-dre <diaso.andre@outlook.com>
2025-08-13 18:11:59 -0300

    perf: improve database connection verification

--- src/drizzle/conn.ts
+++ src/drizzle/conn.ts
@@ -5,7 +5,11 @@ export const db = drizzle(process.env.DATABASE_URL!);
 
 export const check_db = async () => {
   logger.info("Checking database connection...");
-  await db.execute("SELECT 1");
+  try {
+    await db.execute("SELECT 1");
+  } catch (err) {
+    logger.fatal("DATABASE CONNECTION ERROR!");
+    throw err;
+  }
   logger.info("Database connection ok!");
-
-}
+};
