9d00f728daa6748c58a6a5075d22aa905e1a4748
André Dias <diaso.andre@outlook.com>
2025-08-21 18:11:19 +0000

    feat(cryptography): add hmac script

--- src/providers/crypto-provider.ts
+++ src/providers/crypto-provider.ts
@@ -3,18 +3,26 @@ import CryptoJS from "crypto-js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const key = process.env.ENCRYPT_CORE_KEY as string;
-
 export class CryptoProvider {
+  private key = process.env.ENCRYPT_CORE_KEY as string;
+  private blind = process.env.BLIND_KEY as string;
   sha256(payload: string) {
     return crypto.createHash("sha256").update(payload).digest("hex");
   }
+  /**
+   * 
+   * @param payload O valor a ser encriptado
+   * @returns Valor criptografado para blind
+   */
+  hmac(payload: string) {
+    return crypto.createHmac("sha256", this.blind).update(payload).digest("hex");
+  }
   encrypt(value: string) {
-    const encrypted = CryptoJS.AES.encrypt(value, key).toString();
+    const encrypted = CryptoJS.AES.encrypt(value, this.key).toString();
     return encrypted; // Return the encrypted data as a string
   }
   decrypt(encryptedData: string) {
-    const bytes = CryptoJS.AES.decrypt(encryptedData, key); // Decrypt the data
+    const bytes = CryptoJS.AES.decrypt(encryptedData, this.key); // Decrypt the data
     const originalText = bytes.toString(CryptoJS.enc.Utf8); // Convert bytes to UTF-8 string
     return originalText; // Return the original text
   }
