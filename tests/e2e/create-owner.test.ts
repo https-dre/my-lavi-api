872db00073c7111e3f57400b119c491e1d6e73f6
https-dre <diaso.andre@outlook.com>
2025-08-22 00:14:09 -0300

    test: add new owner tests

--- tests/e2e/create-owner.test.ts
+++ tests/e2e/create-owner.test.ts
@@ -1,12 +1,12 @@
-import { describe, it, beforeAll, afterAll, expect } from "vitest";
+import { describe, it, beforeAll, afterAll, afterEach, expect } from "vitest";
 import request from "supertest";
 import { buildApp } from "../../src/buildApp";
-import { config } from "dotenv";
 import { db } from "../../src/drizzle/conn";
-import path from "path";
+import { except } from "drizzle-orm/gel-core";
 
 let app: ReturnType<typeof buildApp>;
 
+// Hooks para todo o arquivo
 beforeAll(async () => {
   app = buildApp();
   await app.ready();
@@ -14,41 +14,57 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await app.close();
-  await db.execute("TRUNCATE laundry, owner RESTART IDENTITY CASCADE")
 });
 
-const new_user = {
+const new_user_data = {
   name: "André Dias",
   email: "diaso.andre@test.com",
   password: "123456789",
-  cep: "06763190",
+  cpf: "12345678902",
+  cep: "12345678",
   birth_date: "2007-05-02",
-  cpf: "54728123804",
 };
 
-describe("POST /owners", () => {
-  it("should create a new owner", async () => {
+describe("Onwer Life Cicle (/owners)", async () => {
+  await db.execute("TRUNCATE owner CASCADE");
+  afterEach(async () => {
+    await db.execute("TRUNCATE owner CASCADE");
+  });
+  it("must create a new owner", async () => {
     const response = await request(app.server)
       .post("/owners")
-      .send({ owner: new_user });
+      .send({ owner: new_user_data });
+
     expect(response.status).toBe(201);
+    expect(response.body.owner_id).toBeDefined();
   });
-});
 
-describe("PUT /owners/auth -> OWNER LOGIN", () => {
-  it("should return JWT and Owner Id", async () => {
-    // create owner
-    await request(app.server)
-      .post("/owners")
-      .send({ owner: new_user });
-
-    // authenticate owner
+  it("must authenticate a owner", async () => {
+    await request(app.server).post("/owners").send({ owner: new_user_data });
     const response = await request(app.server).put("/owners/auth").send({
-      email: new_user.email,
-      password: new_user.password,
+      email: new_user_data.email,
+      password: new_user_data.password,
     });
+
     expect(response.status).toBe(200);
     expect(response.body.token).toBeDefined();
     expect(response.body.owner_id).toBeDefined();
+  });
+
+  it("must return owner data", async () => {
+    const createResponse = await request(app.server)
+      .post("/owners")
+      .send({ owner: new_user_data });
+    console.log(
+      "Resposta recebida da API:",
+      JSON.stringify(createResponse.body, null, 2)
+    );
+
+    expect(createResponse.status).toBe(201);
+    const ownerId = createResponse.body.owner_id;
+
+    const response = await request(app.server).get(`/owners/${ownerId}`);
+    expect(response.status).toBe(200);
+    expect(response.body.owner.id).toBe(ownerId);
   });
 });
