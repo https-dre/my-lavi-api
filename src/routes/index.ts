b4a2e335582d39176748e08e27afd11665d4d844
https-dre <diaso.andre@outlook.com>
2025-08-25 19:18:42 -0300

    feat(orders): add orders router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { owner_routes } from "./owner-routes";
 import { customer_routes } from "./customer-routes";
 import { laundry_routes } from "./laundry-routes";
+import { order_router } from "./order-routes";
 
 export const router = async (server: FastifyInstance) => {
   const app = server.withTypeProvider<ZodTypeProvider>();
@@ -18,4 +19,5 @@ export const router = async (server: FastifyInstance) => {
   owner_routes(app);
   customer_routes(app);
   laundry_routes(app);
+  order_router(app);
 };
