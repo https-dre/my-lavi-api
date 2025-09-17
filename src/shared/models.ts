2e6b30c140fec549cbc74f5dd24ee78daaee1f7d
https-dre <diaso.andre@outlook.com>
2025-09-17 20:09:58 -0300

    feat(owner): add 'Required' flag to make all fields required

--- src/shared/models.ts
+++ src/shared/models.ts
@@ -1,14 +1,14 @@
-import tables from './drizzle/tables';
+import tables from "./drizzle/tables";
 
 export type CustomerModel = typeof tables.customer.$inferInsert;
 
 export type CustomerAddressModel = typeof tables.customerAddress.$inferInsert;
 
-export type OwnerModel = typeof tables.owner.$inferInsert;
+export type OwnerModel = Required<typeof tables.owner.$inferInsert>;
 
 export type LaundryModel = typeof tables.laundry.$inferInsert;
 
-export type LaundryBannerModel = typeof tables.laundryBanner.$inferInsert
+export type LaundryBannerModel = typeof tables.laundryBanner.$inferInsert;
 
 export type EmployeeModel = typeof tables.employee.$inferInsert;
 
@@ -19,3 +19,4 @@ export type OrderItemModel = typeof tables.orderItem.$inferInsert;
 export type FeedbackModel = typeof tables.feedbackPost.$inferInsert;
 
 export type FeedbackImageModel = typeof tables.feedbackImage.$inferInsert;
+
