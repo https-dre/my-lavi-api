cadab84fd6f2b05e478e8d4ac852339fb04dacfc
https-dre <diaso.andre@outlook.com>
2025-09-18 17:17:49 -0300

    feat: update DTOs

--- src/shared/dto/index.ts
+++ src/shared/dto/index.ts
@@ -3,10 +3,11 @@ import z from "zod";
 import { RemoveSensitiveFields } from "../functions/remove-sensitive-fields";
 import { CustomerModel, LaundryModel, OwnerModel } from "../models";
 import { ZodOrder } from "./zod-schemas";
+import { CustomerType, OwnerType } from "./typebox";
 
-export type OwnerDTO = RemoveSensitiveFields<OwnerModel>;
+export type OwnerDTO = typeof OwnerType.static;
 
-export type CustomerDTO = RemoveSensitiveFields<CustomerModel>;
+export type CustomerDTO = typeof CustomerType.static;
 
 export type LaundryDTO = RemoveSensitiveFields<LaundryModel>;
 
