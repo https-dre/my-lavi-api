f287a81f222485ac3587b252be57171d0c08c890
https-dre <diaso.andre@outlook.com>
2025-09-17 22:18:05 -0300

    fix(drizzle): update schema location

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/shared/drizzle/tables.ts",
+  schema: "./database/tables.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
