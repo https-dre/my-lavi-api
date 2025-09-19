e91c2b92d8286c89176a36b22909a2f38e326661
André Dias <diaso.andre@outlook.com>
2025-09-19 11:12:42 +0000

    feat(orders): add new route to push order items

--- src/order/routes/index.ts
+++ src/order/routes/index.ts
@@ -2,9 +2,11 @@ import Elysia from "elysia";
 import { createOrder } from "./create-order";
 import { deleteOrder } from "./delete-order";
 import { appServices } from "../../shared/services";
+import { pushOrderItems } from "./push-order-items";
 
 const orderController = new Elysia()
   .use(createOrder(appServices.order))
-  .use(deleteOrder(appServices.order));
+  .use(deleteOrder(appServices.order))
+  .use(pushOrderItems(appServices.order));
 
 export { orderController };
---
+++ src/order/routes/push-order-items.ts
@@ -0,0 +1,28 @@
+import Elysia, { t } from "elysia";
+import { OrderService } from "../order-service";
+import { OrderItemType } from "../../shared/dto/typebox";
+
+export const pushOrderItems = (service: OrderService): Elysia => {
+  return new Elysia().post(
+    "/orders/:id/items",
+    async ({ body, status }) => {
+      const { items } = body;
+      const items_created = await service.pushOrderItems(items);
+      return status(201, { items_created });
+    },
+    {
+      detail: {
+        summary: "Push order items",
+        tags: ["orders"],
+      },
+      body: t.Object({
+        items: t.Array(t.Omit(OrderItemType, ["id"])),
+      }),
+      response: {
+        201: t.Object({
+          items_created: t.Array(OrderItemType),
+        }),
+      },
+    }
+  );
+};
