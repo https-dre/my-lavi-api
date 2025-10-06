5858b5a012fe9e24658548d0f5d672a7bd0980ef
https-dre <diaso.andre@outlook.com>
2025-10-06 19:01:48 -0300

    improve migration system with production or development mode

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "drizzle-kit";
 
+import * as dotenv from "dotenv";
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+dotenv.config({ path: envFile });
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/database/tables.ts",
