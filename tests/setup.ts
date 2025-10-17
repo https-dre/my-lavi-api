c9865ecde6ab28eb09f2ffdac3588a4ba5353055
André Dias <diaso.andre@outlook.com>
2025-10-17 16:30:40 +0000

    refactor: move files to 'api' folder

--- tests/setup.ts
+++
@@ -1,7 +0,0 @@
-// tests/setup.ts
-import { config } from 'dotenv';
-import path from 'path';
-
-// Como os testes rodam com NODE_ENV=test, carregamos o .env mas
-// nosso código de conexão vai priorizar a DATABASE_URL_TEST
-config({ path: path.resolve(process.cwd(), '.env') });
